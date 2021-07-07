759549278c0b56e8ae4a3c450dcb9b3929b73d19
nobishino <nobishii11@gmail.com>
2021-07-07 22:43:54 +0900

    Chapter 2.4 p32 FileServerとHandlerFuncをmultiplexする

--- main.go
+++ main.go
@@ -5,11 +5,18 @@ import (
 	"net/http"
 )
 
-func handler(writer http.ResponseWriter, request *http.Request) {
+func index(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello, World, %s!", request.URL.Path[1:])
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir("./public"))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+	mux.HandleFunc("/", index)
+	server := &http.Server{
+		Addr:    "0.0.0.0:8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
 }
