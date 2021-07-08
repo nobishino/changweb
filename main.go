12c16489cccb59a002cc1ae67959eb9eb0abefda
nobishino <nobishii11@gmail.com>
2021-07-08 19:56:38 +0900

    Chapter3, p62 何もしない

--- main.go
+++ main.go
@@ -10,13 +10,5 @@ func index(writer http.ResponseWriter, request *http.Request) {
 }
 
 func main() {
-	mux := http.NewServeMux()
-	files := http.FileServer(http.Dir("./public"))
-	mux.Handle("/static/", http.StripPrefix("/static/", files))
-	mux.HandleFunc("/", index)
-	server := &http.Server{
-		Addr:    "0.0.0.0:8080",
-		Handler: mux,
-	}
-	server.ListenAndServe()
+	http.ListenAndServe("", nil)
 }
