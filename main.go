d781cf5c9bdd32b227748f9181905eb2f2e7af43
nobishino <nobishii11@gmail.com>
2021-07-08 20:16:05 +0900

    ハンドラを追加する

--- main.go
+++ main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	index(w, r)
+}
+
 func index(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "Hello, World, %s!", request.URL.Path[1:])
 }
@@ -12,7 +18,7 @@ func index(writer http.ResponseWriter, request *http.Request) {
 func main() {
 	server := http.Server{
 		Addr:    "127.0.0.1:8080",
-		Handler: nil,
+		Handler: new(MyHandler),
 	}
 	server.ListenAndServeTLS("cert.pem", "key.pem")
 }
