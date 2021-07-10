db45c425001951da677ec2b3d7f11e86662dd1ba
nobishino <nobishii11@gmail.com>
2021-07-10 22:30:45 +0900

    http.Request.Header

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/nobishino/changweb
 
 go 1.16
-
-require github.com/julienschmidt/httprouter v1.3.0
--- main.go
+++ main.go
@@ -3,22 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-func hello(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	fmt.Fprintf(writer, "Hello, %s!\n", params.ByName("name"))
+func hello(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, request.Header)
 }
 
 func main() {
-	mux := httprouter.New()
-	mux.GET("/hello/:name", hello)
 
 	server := http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: mux,
+		Addr: "127.0.0.1:8080",
 	}
 
+	http.HandleFunc("/", hello)
+
 	server.ListenAndServe()
+	// server.ListenAndServeTLS("cert.pem", "key.pem")
 }
