3afdc5d6ae86441fcb923b11ccc354c193f39ab2
nobishino <nobishii11@gmail.com>
2021-07-11 11:38:19 +0900

    FileUpload, MultiPartForm.File

--- main.go
+++ main.go
@@ -33,7 +33,14 @@ func body(writer http.ResponseWriter, request *http.Request) {
 
 func process(w http.ResponseWriter, r *http.Request) {
 	r.ParseMultipartForm(1024)
-	fmt.Fprintln(w, r.MultipartForm)
+	fileHeader := r.MultipartForm.File["uploaded"][0]
+	f, err := fileHeader.Open()
+	if err != nil {
+		fmt.Fprintln(w, err)
+		return
+	}
+	defer f.Close()
+	io.Copy(w, f)
 }
 
 func main() {
