3b3b5ccac7cb263bae696979ca5852cda39a8c97
nobishino <nobishii11@gmail.com>
2021-07-15 20:00:21 +0900

    エスケープ処理

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ import (
 //go:embed templates/*
 var templates embed.FS
 
+//go:embed client.html
+var static embed.FS
+
 func formatDate(t time.Time) string {
 	format := "20060102"
 	return t.Format(format)
@@ -28,8 +31,21 @@ func process(w http.ResponseWriter, req *http.Request) {
 	t.Execute(w, time.Now())
 }
 
+func process2(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.New("template.html").ParseFS(templates, "templates/template.html")
+	t.Execute(w, req.FormValue("comment"))
+}
+
+func form(w http.ResponseWriter, req *http.Request) {
+	t, _ := template.New("form.html").ParseFS(templates, "templates/form.html")
+	t.Execute(w, nil)
+}
+
 func main() {
-	http.HandleFunc("/", process)
+	static := http.FileServer(http.FS(static))
+	http.Handle("/", static)
+	http.HandleFunc("/process", process2)
+	http.HandleFunc("/form", form)
 
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
